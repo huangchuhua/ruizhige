d2e63c22442441f26523042575467d161784d706
huangchuhua <chuhua_huang@126.com>
2017-08-30 17:59:58 +0800

    完成首页和登录页面的路由功能

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,20 @@
 /**主模块 */
-angular.module("app", ["aside"])
-// angular.module("app", [
-//         "ngRoute",
-//         "aside",
-//         "header",
-//         "login",
-//         "index"
-//     ])
-//     .config(["$routeProvider", function ($routeProvider) {
-//         $routeProvider
-//             .when("/", {
-//                 template: ""
-//             })
-//             .when("/index", {
-//                 redirectTo: "/"
-//             })
-//             .when("/login", {
-//                 template: ""
-//             })
-//             .otherwise({
-//                 templateUrl: "/src/html/404.html"
-//             })
-//     }])
+
+
+
+angular.module("app", ["ngRoute", "aside", "header", "index", "login"])
+    .config(["$routeProvider", function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                template: "<ngl-aside></ngl-aside><ngl-index></ngl-index>"
+            })
+            .when("/index", {
+                redirectTo: "/"
+            })
+            .when("/login", {
+                template: "<ngl-login></ngl-login>"
+            })
+            .otherwise({
+                templateUrl: "/src/html/404.html"
+            })
+    }])
